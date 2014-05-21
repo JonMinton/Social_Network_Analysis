b7f9e423780770889ee5ee0f806c4d80de94f689
JonMinton <nate.minton@gmail.com>
2014-05-21 14:51:16 +0100

    Add Notes directory including mm

---
+++ Notes/Social Network Analysis Project.mm
@@ -0,0 +1,36 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1400677591296" ID="ID_1838286313" MODIFIED="1400677612961" TEXT="Social Network Analysis Project">
+<node CREATED="1400677614660" ID="ID_1910278327" MODIFIED="1400677616176" POSITION="right" TEXT="Notes"/>
+<node CREATED="1400677616611" ID="ID_275502712" MODIFIED="1400677618608" POSITION="right" TEXT="Background"/>
+<node CREATED="1400677618859" ID="ID_446286066" MODIFIED="1400677622664" POSITION="right" TEXT="Deadlines"/>
+<node CREATED="1400677622883" ID="ID_1880801428" MODIFIED="1400677624391" POSITION="right" TEXT="Commitments"/>
+<node CREATED="1400677624659" ID="ID_1081782902" MODIFIED="1400677627143" POSITION="right" TEXT="Deliverables"/>
+<node CREATED="1400677681855" ID="ID_449802906" MODIFIED="1400677692971" POSITION="right" TEXT="Acronyms">
+<node CREATED="1400677694694" ID="ID_821928417" MODIFIED="1400677697979" TEXT="BDUIC">
+<node CREATED="1400677700358" ID="ID_751746164" MODIFIED="1400677708586" TEXT="Big Data and Urban Informatics"/>
+</node>
+</node>
+<node CREATED="1400677716829" ID="ID_393509437" MODIFIED="1400677725481" POSITION="right" TEXT="Workshops">
+<node CREATED="1400677726172" ID="ID_849883611" MODIFIED="1400677732465" TEXT="BUUIC">
+<node CREATED="1400677737292" ID="ID_1127061114" MODIFIED="1400677745560" TEXT="August 11-12, 2014"/>
+<node CREATED="1400677748907" ID="ID_972735443" MODIFIED="1400677753583" TEXT="Title">
+<node CREATED="1400677754651" ID="ID_105282077" MODIFIED="1400677787301" TEXT="&quot;The effect of ethnic mix on the marketing language of residential property: A social network analysis&quot;"/>
+</node>
+<node CREATED="1400677831350" ID="ID_756974536" MODIFIED="1400677832922" TEXT="Abstract">
+<node CREATED="1400677835237" MODIFIED="1400677835237">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <span style="font-size: 14pt; font-family: Garamond, serif"><font color="rgb(34, 34, 34)" size="14pt" face="Garamond, serif"><b style="letter-spacing: normal; font-size: 13px; text-transform: none; white-space: normal; text-align: start; font-variant: normal; background-color: rgb(255, 255, 255); text-indent: 0px; line-height: normal; word-spacing: 0px; font-family: arial, sans-serif; font-style: normal; color: rgb(34, 34, 34)">Abstract:</b></font></span><font color="rgb(34, 34, 34)" size="14pt" face="Garamond, serif"><span class="Apple-converted-space">&#160;</span><span style="letter-spacing: normal; font-size: 14pt; text-transform: none; text-align: start; white-space: normal; font-weight: normal; font-variant: normal; background-color: rgb(255, 255, 255); text-indent: 0px; word-spacing: 0px; line-height: normal; font-family: Garamond, serif; font-style: normal; color: rgb(34, 34, 34)">This paper conceptualises real estate marketing language formation as a social network. We use this theoretical framework, and the fact that estate agents seek to reflect buyer aspirations, to interpret patterns of language as indicators of social perceptions of an area. Data on a large number of house transactions are then used to explore the relationship between the ethnic mix of an area and the language used, using a breakdown of seller hyperbole based on Aristotle&#8217;s theory of rhetoric. Because of the interdependence of estate agent language variation proposed by the social network framework, we then relax the assumption of independence between transactions and apply social network analysis to language variation over time exploring the degree of homophily&#8212;the extent to which similar dwellings in similar neighbourhoods have similar patterns of marketing language. We use the model to investigate the particular question of whether changes in the ethnic and religious mix of a neighbourhood lead to measureable changes in the co-movement of language variation between areas.</span></font>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
