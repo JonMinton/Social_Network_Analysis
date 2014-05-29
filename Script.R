c48d0e1852edd97669cf1f8514053a3d3154e373
Jonathan Minton <nate.minton@gmail.com>
2014-05-29 23:02:15 +0100

    Add additional checks and work to Load_and_Prepare_Data. Add additional dependencies to start of main script

--- Script.R
+++ Script.R
@@ -12,6 +12,7 @@ require(statnet)
 require(foreign)
 require(RCurl)
 require(httr)
+require(igraph)
 
 # Source files
 
@@ -34,21 +35,96 @@ source("Scripts/Load_and_Prepare_Data.R")
 #   4) Summary states of covariates 
 
 
-# The column numbers containing the covariates 
-varcols <- 3:(dim(Data)[2] - dim(Data)[1]) + dim(Data)[1]
-Data.covariates <- Extract_Covariates(
-    input=Data,
-    postcode.loc=2,
-    cov.loc = varcols
-    )
-rm(varcols)
 
 
-# Note: the covariates data only seems to start at row 60. A bug?
 
-Data.sociomatrix <- Extract_SNA_Matrix(
-    Data
-    )
 
+
+
+
+
+
+cpep.network<-network(complete.cpep,vertex.attr=cpep.attrib,directed=F,matrix.type="adjacency")
+
+
+
+
+### Additional data promised from code:
+
+# CPEP_SNA_Results/CPEPmergedwithOther.RData
+
+
+### Additional code from Nema 2
+
+setwd("Non-public/Housing_Project/CPEP_SNA_Results")
+load("CPEPmergedwithOther.RData")
+#Get rid of entries with at least 1 missing covariate
+complete.dataset<-na.omit(full.datast)
+#Need to extract the corresponding columns
+col.select<-as.numeric(rownames(complete.dataset))+1
+col.select<-c(col.select,10059:10080)
+rownames(complete.dataset)<-complete.dataset[,1]
+complete.dataset<-complete.dataset[,col.select]
+n<-nrow(complete.dataset)
+d<-ncol(complete.dataset)
+
+
+#Separate into adjacency matrix and list of attributes
+complete.cpep<-complete.dataset[,1:n]
+cpep.attrib<-as.list(complete.dataset[,((n+1):d)])
+# A list where all elements have the same length is a data.frame, isn't it?
+
+#Create network object
+save.image("CPEPnetworkobject.RData")
+#cpep.network<-network(cpepbinary.new,vertex.attr=Ext.var.list,vertex.attrnames=Ext.var.names,directed=F,matrix.type="adjacency")
+model1<-ergm(cpep.network~edges+triangle)
+save.image("CPEP_SNA/CPEPergmRes.RData")
+
+model2<-ergm(cpep.network~edges+triangle+absdiff("flats")+absdiff("dombed"))
+
+
+### Additional code from Nema 3
+
+setwd("Non-public/Housing_Project/CPEP_SNA_Results")
+load("CPEPmergedwithOther.RData")
+#Get rid of entries with at least 1 missing covariate
+complete.dataset<-na.omit(full.datast)
+#Need to extract the corresponding columns
+col.select<-as.numeric(rownames(complete.dataset))+1
+col.select<-c(col.select,10059:10080)
+rownames(complete.dataset)<-complete.dataset[,1]
+complete.dataset<-complete.dataset[,col.select]
+n<-nrow(complete.dataset)
+d<-ncol(complete.dataset)
+#Separate into adjacency matrix and list of attributes
+complete.cpep<-complete.dataset[,1:n]
+cpep.attrib<-as.list(complete.dataset[,((n+1):d)])
+
+
+
+cpep.igraph<-graph.adjacency(complete.cpep,"undirected")
+#Calculate some summary graph measures: how many connected components, graph density and node closeness
+gr.no.comps<-no.clusters(cpep.igraph)
+gr.density<-graph.density(cpep.igraph)
+node.closeness<-closeness(cpep.igraph)
+mean(node.closeness)
+node.betweenness<-betweenness(cpep.igraph,directed=F)
+summary(node.betweenness)
+graph.transitivity<-transitivity(cpep.igraph,"globalundirected")
+graph.transitivity
+node.centrality.sc<-centralization.betweenness(cpep.igraph, directed=F)
+node.centrality.sc$central
+
+######################
+#Homophily
+#####################
+homoph.meas<-matrix(NA,1,length(cpep.attrib))
+colnames(homoph.meas)<-names(cpep.attrib)
+#Calculate homophily measures
+for(i in 1:length(cpep.attrib))
+{
+    homoph.meas[1,i]<-assortativity(cpep.igraph,cpep.attrib[[i]],directed=F)
+    #	print(i)
+}
 
 
--- Scripts/Load_and_Prepare_Data.R
+++ Scripts/Load_and_Prepare_Data.R
@@ -5,23 +5,69 @@
 # on some machines due to memory limits being reached. However I cannot improve on it for now. 
 
 
-if (!file.exists("Data/Raw/Full_Dataset.csv")){
-    url <- "https://www.dropbox.com/s/3tqfbty79i2hpdl/full.dataset.csv"
-    print("Cannot find dataset. Downloading.")
-    dir.create("Data/Raw/", recursive=T, showWarnings=F)
-    tmp <- httr::GET(url,
-                     verbose()
-    )
-    writeBin(
-        content(tmp, "raw"),
-        "Data/Raw/Full_Dataset.csv"
-    )
+# Order of checks
+# 1) Has the Robject Data been found?
+# 2) Have the derived R objects Data.E and Data.V been identified?
+# 3) has the CSV dataset Full_Dataset.csv been identified?
+
+if (!file.exists("Data/RObj/Data.RData")){
+    print("Data not found as R object. Searching for CSV file locally")
+    
+    if (!file.exists("Data/Raw/Full_Dataset.csv")){
+        url <- "https://www.dropbox.com/s/3tqfbty79i2hpdl/full.dataset.csv"
+        print("Cannot find dataset. Downloading.")
+        dir.create("Data/Raw/", recursive=T, showWarnings=F)
+        tmp <- httr::GET(url,
+                         verbose()
+        )
+        writeBin(
+            content(tmp, "raw"),
+            "Data/Raw/Full_Dataset.csv"
+        )
+    } else {
+        print("Found Dataset as CSV locally, so not downloading again.")
+    }
+    
+    
+    # Read the file into an R object format
+    print("Loading Data from CSV")
+    
+    Data <- read.csv("Data/Raw/Full_Dataset.csv")
 } else {
-    print("Found Dataset so not downloading again.")
+    print("Data found as R Objects. Loading workspace")
+    
+    load("Data/RObj/Data.RData")
+    
 }
 
+print("Seeing whether data have been unpacked into Edges and Vertices")
+if (!exists(Data.V) | !exists(Data.E)){
+    
+    if (!exists(Data.V)){
+        print("Vertex covariate data not found. Unpacking from Data now")
+        # The column numbers containing the covariates 
+        varcols <- 3:(dim(Data)[2] - dim(Data)[1]) + dim(Data)[1]
+        Data.V <- Extract_Covariates(
+            input=Data,
+            postcode.loc=2,
+            cov.loc = varcols
+        )
+        rm(varcols)
+        
+    } 
+    if (!exists(Data.E)){
+        print("Edge (Sociomatrix) data not found. Unpackaging from Data now")
+        
+        Data.sociomatrix <- Extract_SNA_Matrix(
+            Data
+        )
+    }
+    print("Saving unpacked data as Data.RData")
+    # Saving Unpacked Data as Data.RData
+    save(Data, Data.E, Data.V,
+         file="Data/Robj/Data.RData"
+         )
+} else {
+    print("All data appear to have been loaded already.")
+}
 
-# Read the file into an R object format
-
-
-Data <- read.csv("Data/Raw/Full_Dataset.csv")
