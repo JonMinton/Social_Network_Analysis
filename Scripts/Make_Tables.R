815c28eddce08e2d2debce8bf7f07f2f56cfc1c0
Jonathan Minton <nate.minton@gmail.com>
2014-06-05 09:55:43 +0100

    New table. Slight changes to Script.

--- Scripts/Make_Tables.R
+++ Scripts/Make_Tables.R
@@ -4,3 +4,8 @@ tmp <- xtable(ddply(Data_Long, year~variable, summarise, mean=mean(value), sd=sd
 print.xtable(tmp, file="Tables/Pathos_Summary.html", type="html")
 rm(tmp)
 
+print.xtable(
+    xtable(head(Pathos_Data), label="Sample of Pathos_Data", digits=2),
+    file="Tables/Pathos_Data_Sample.html", type="html"
+    )
+
